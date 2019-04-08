b122cc998b3754288d47472ac65f1d9676d46c8b
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-08 19:27:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   sq_results = [ ]
-  array.each  {|number| sq_results.push(number**2)}
+  array.each  {|number| sq_results<<(number**2)}
     return sq_results
-  end
 end
+
 
 def collect_square(array)
   array.collect do|num|
